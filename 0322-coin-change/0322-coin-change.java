ec3f58cd60039a009a822d9a559b085463f76780
Ankit kumar <121846044+Ankittt81@users.noreply.github.com>
2026-01-25 00:46:40 +0530

    0322-coin-change

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
         int[] dp=new int[amount+1];
-        Arrays.fill(dp,-1);
-        int res=mincoin(amount,coins,dp);
-        return (res==Integer.MAX_VALUE?-1:res);
-    }
-    int mincoin(int amount,int[] coins,int[] dp){
-        if(amount==0) return dp[amount]=0;
-        if(amount<0) return Integer.MAX_VALUE;
-        if(dp[amount]!=-1) return dp[amount];
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<coins.length;i++){
-            int res=mincoin(amount-coins[i],coins,dp);
-            if(res!=Integer.MAX_VALUE){
-                 ans=Math.min(ans,res+1);
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int coin:coins){
+                if(coin<=i){
+                    dp[i]=Math.min(dp[i],dp[i-coin]+1);
+                }
             }
-           
         }
-        return dp[amount]=ans;
+        return (dp[amount]==amount+1)?-1:dp[amount];
     }
 }
