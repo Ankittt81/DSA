d92721990525a5362e148c53323717cc82ba45a4
Ankit kumar <121846044+Ankittt81@users.noreply.github.com>
2026-02-02 03:01:34 +0530

    0451-sort-characters-by-frequency

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -6,23 +6,15 @@ class Solution {
             char c=s.charAt(i);
             map.put(c,map.getOrDefault(c,0)+1);
         }
-        List<Character>[] bucketList=new ArrayList[n+1];
-        for(char c:map.keySet()){
-            int freq=map.get(c);
-            if(bucketList[freq]==null){
-                bucketList[freq]=new ArrayList<>();
-            }
-            bucketList[freq].add(c);
+       List<Character>list=new ArrayList<>(map.keySet());
+       list.sort((a,b)->map.get(b)-map.get(a));
+
+       StringBuilder sb=new StringBuilder();
+       for(char c:list){
+        for(int i=0;i<map.get(c);i++){
+            sb.append(c);
         }
-        StringBuilder sb=new StringBuilder();
-        for(int i=bucketList.length-1;i>=0;i--){
-            if(bucketList[i]!=null){
-                for(char c:bucketList[i]){
-                    for(int k=1;k<=i;k++)
-                    sb.append(c);
-                }
-            }
-        }
-        return sb.toString();
+       }
+       return sb.toString();
     }
 }
