2155268beb7dd349b6d0fa1178d870423e978bfb
Ankit kumar <121846044+Ankittt81@users.noreply.github.com>
2026-02-12 22:23:22 +0530

    3713-longest-balanced-substring-i

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int n=s.length();
+        int maxLen=0;
+        for(int i=0;i<n;i++){
+            int[] freq=new int[26];
+            int distinct=0;
+            int maxfreq=0;
+            for(int j=i;j<n;j++){
+                int index=s.charAt(j)-'a';
+
+                if(freq[index]==0) distinct++;
+
+                freq[index]++;
+                maxfreq=Math.max(maxfreq,freq[index]);
+
+                int length=j-i+1;
+                if(length==distinct*maxfreq){
+                    maxLen=Math.max(maxLen,length);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
